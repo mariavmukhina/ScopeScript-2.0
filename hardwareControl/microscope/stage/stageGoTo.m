8986ce456a4dd4e8088077248c09223e5f134e93
Maria Mukhina <mmuxina@gmail.com>
2026-03-24 10:57:04 -0400

    Update stageGoTo.m  for live regime - needs TESTING!!!

--- hardwareControl/microscope/stage/stageGoTo.m
+++ hardwareControl/microscope/stage/stageGoTo.m
@@ -1,11 +1,9 @@
-function [] = stageGoTo(stagePos)
-%STAGEGOTO moves the microscope stage to XY position from the stageCoordinates list, given that stageCoordinates is populated by stageAppend
+function [] = stageGoTo(positionN)
+%STAGEGOTO moves the microscope stage to XYZ positionN from the stagePos list saved in object stageCoordinates of class scopeParams, given that stagePos is populated by stageAppend
 
 global stageCoordinates;
-if stagePos > 0 && stagePos <= numel(stageCoordinates)
-gotoFcScope = stageCoordinates{stagePos};
-parsedAndValues = parseParamsForFunctions(gotoFcScope);
-gotoStagePos(parsedAndValues.stagePos);
+if positionN > 0 && positionN <= numel(stageCoordinates.stagePos)
+   gotoStagePos(stageCoordinates.stagePos(positionN));
 else
    disp('stage position is out of bounds'); 
 end
